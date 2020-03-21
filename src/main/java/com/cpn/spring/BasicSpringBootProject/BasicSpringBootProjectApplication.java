115c2b1765b330581b5da1030b8afa87d5fed5cc
cassiopereira.nunes@gmail.com <cassiopereira.nunes@gmail.com>
2020-03-21 21:37:35 +0000

    Basic Webservice

--- src/main/java/com/cpn/spring/BasicSpringBootProject/BasicSpringBootProjectApplication.java
+++ src/main/java/com/cpn/spring/BasicSpringBootProject/BasicSpringBootProjectApplication.java
@@ -2,12 +2,18 @@ package com.cpn.spring.BasicSpringBootProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BasicSpringBootProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicSpringBootProjectApplication.class, args);
+		ApplicationContext applicationContext =
+				SpringApplication.run(BasicSpringBootProjectApplication.class, args);
+
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
