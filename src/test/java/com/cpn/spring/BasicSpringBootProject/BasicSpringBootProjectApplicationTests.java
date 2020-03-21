5c5371b1429b198da375fc171381da4a972ed1ea
cassiopereira.nunes@gmail.com <cassiopereira.nunes@gmail.com>
2020-03-21 20:20:39 +0000

    INitital version

---
+++ src/test/java/com/cpn/spring/BasicSpringBootProject/BasicSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpn.spring.BasicSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
